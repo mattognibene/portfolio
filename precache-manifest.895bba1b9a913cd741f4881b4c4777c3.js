acef025db3f253538fbe11ad45b10cae5426c2d5
Matthew Ognibene <mattognibene1@gmail.com>
2020-05-19 22:05:56 -0400

    Updates

---
+++ precache-manifest.895bba1b9a913cd741f4881b4c4777c3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c23e520ebc3690296763104132c7e06",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "77c55d9e4ebe37cd4e65",
+    "url": "/portfolio/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "8d2369ea24f193302afe",
+    "url": "/portfolio/static/css/main.597ab11e.chunk.css"
+  },
+  {
+    "revision": "77c55d9e4ebe37cd4e65",
+    "url": "/portfolio/static/js/2.61ff3c83.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio/static/js/2.61ff3c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2369ea24f193302afe",
+    "url": "/portfolio/static/js/main.d67b859d.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "40ce31a446f7e893ad9959c5018ea7b1",
+    "url": "/portfolio/static/media/android.40ce31a4.svg"
+  },
+  {
+    "revision": "b675af6016eed403de51899be75efe59",
+    "url": "/portfolio/static/media/css3.b675af60.svg"
+  },
+  {
+    "revision": "92a65334db083442496c5868b57c9ae9",
+    "url": "/portfolio/static/media/docker.92a65334.svg"
+  },
+  {
+    "revision": "df8bf7ad749ccc8ae99516952a8f2878",
+    "url": "/portfolio/static/media/firebase.df8bf7ad.svg"
+  },
+  {
+    "revision": "b73214e3ebad909ef6891be949c39947",
+    "url": "/portfolio/static/media/git.b73214e3.svg"
+  },
+  {
+    "revision": "ee561eb5c6140a366b4a3e006214cb84",
+    "url": "/portfolio/static/media/gnubash.ee561eb5.svg"
+  },
+  {
+    "revision": "3056bf2f96c40cd0a814b355959a6983",
+    "url": "/portfolio/static/media/html5.3056bf2f.svg"
+  },
+  {
+    "revision": "fa1652e4fa09bc238156083b268be747",
+    "url": "/portfolio/static/media/ic_baked_up.fa1652e4.jpg"
+  },
+  {
+    "revision": "d73c7d19712053eabecbedcf0aee29d0",
+    "url": "/portfolio/static/media/ic_cookie.d73c7d19.png"
+  },
+  {
+    "revision": "7d0fbdc13dd07f2cdb3018a746ea2fba",
+    "url": "/portfolio/static/media/ic_delt.7d0fbdc1.jpg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/portfolio/static/media/ic_github.617870e4.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/portfolio/static/media/ic_linkedin.619231ff.svg"
+  },
+  {
+    "revision": "f6ed2241065f14082bcc8490960df40a",
+    "url": "/portfolio/static/media/ic_mail.f6ed2241.svg"
+  },
+  {
+    "revision": "6ef52c6afee64abb85ac4ec5e40e065d",
+    "url": "/portfolio/static/media/ic_next.6ef52c6a.svg"
+  },
+  {
+    "revision": "f806e1ddbc732a8d193c8d927f669ad8",
+    "url": "/portfolio/static/media/ic_stoke.f806e1dd.png"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/portfolio/static/media/ic_twitter.be066898.svg"
+  },
+  {
+    "revision": "95c07d5d563027ee67932cfdffc0cbe8",
+    "url": "/portfolio/static/media/java.95c07d5d.svg"
+  },
+  {
+    "revision": "1f6069572c9380a733700436d2980f8a",
+    "url": "/portfolio/static/media/javascript.1f606957.svg"
+  },
+  {
+    "revision": "1a2eaf4220f1fa06dc2a57c656ffa8c2",
+    "url": "/portfolio/static/media/kotlin.1a2eaf42.svg"
+  },
+  {
+    "revision": "0de4d37b6f6da93ec8a95e6035702671",
+    "url": "/portfolio/static/media/me.0de4d37b.jpg"
+  },
+  {
+    "revision": "34cbdb156ea57b239962b2909e0a6d89",
+    "url": "/portfolio/static/media/postgresql.34cbdb15.svg"
+  },
+  {
+    "revision": "59c0303dc0d73aadd433bd82c724070c",
+    "url": "/portfolio/static/media/python.59c0303d.svg"
+  },
+  {
+    "revision": "092e0882358cd09dbe9376b64d0f2a0e",
+    "url": "/portfolio/static/media/react.092e0882.svg"
+  },
+  {
+    "revision": "23231f8bdb7acaa0f45c898b7afa5043",
+    "url": "/portfolio/static/media/whoop_logo.23231f8b.jpeg"
+  }
+]);
